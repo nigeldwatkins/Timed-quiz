79b1981dde5f524ed87fe5cc8746b564571c509b
nigeldwatkins <nigeldwatkins@gmail.com>
2023-04-20 20:13:34 -0400

    updated

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,73 @@
-// Adding the timer element to my script, 60seconds
 var timeleft = 60;
 var countdownTimer;
+var currentQuestion = 0;
+
+const questions = [
+  {
+    question: "If the condition in the 'If statement is false you should create a new variable with 'var'",
+    choices: ["True", "False"],
+    answer: "False"
+  },
+];
 
 document.getElementById("start-btn").addEventListener("click", function() {
-    countdownTimer = setInterval(function() {
-        timeleft--;
-        document.getElementById("timer").innerHTML = timeleft;
-        if(timeleft <= 0) {
-        clearInterval(countdownTimer);
-        alert("Quiz timer up!");
+  countdownTimer = setInterval(function() {
+    timeleft--;
+    document.getElementById("timer").innerHTML = timeleft;
+    
+    if (timeleft < 0) {
+      clearInterval(countdownTimer);
+      alert("Quiz timer up!");
     }
- }, 1000); 
+  }, 1000);
+
+  const quizContainer = document.getElementById("container");
+  const startButton = document.getElementById("start-btn");
+  const questionElement = document.getElementById("quiz-questions");
+  const answersElement = document.getElementById("quiz-answers");
+
+  startButton.addEventListener("click", startQuiz);
+
+  function startQuiz() {
+    startButton.style.display = "none";
+    displayQuestion();
+  }
+
+  function displayQuestion() {
+    const questionObj = questions[currentQuestion];
+    questionElement.textContent = questionObj.question;
+    answersElement.innerHTML = "";
+
+    questionObj.choices.forEach(choice => {
+      const button = document.createElement("button");
+      button.textContent = choice;
+      button.addEventListener("click", () => {
+        checkAnswer(choice, questionObj.answer);
+      });
+      answersElement.appendChild(button);
+    });
+  }
+
+  function checkAnswer(choice, answer) {
+    if (choice === answer) {
+      alert("Correct!");
+    } else {
+      alert("Incorrect!");
+      timeleft -=5;
+    }
+
+    currentQuestion++;
+    if (currentQuestion < questions.length) {
+      displayQuestion();
+    } else {
+      endQuiz();
+    }
+  }
+
+  function endQuiz() {
+    clearInterval(countdownTimer);
+    alert("Quiz is over!");
+  }
 });
 
 
